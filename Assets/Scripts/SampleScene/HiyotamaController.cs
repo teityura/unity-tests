f6db50c89162d36751d904ca4d2295b229435462
teityura <teityura@gmail.com>
2021-09-05 22:08:03 +0900

    hiyotama test

---
+++ Assets/Scripts/SampleScene/HiyotamaController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiyotamaController : MonoBehaviour
+{
+	[SerializeField] Animator hiyotamaAnimator;
+	private bool IsCry;
+
+    void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			hiyotamaAnimator.SetBool("Cry", IsCry);
+			IsCry = !IsCry;
+		}
+    }
+}
